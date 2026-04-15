80f5db7a9d854f3dcfb61d35242e187da3932a14
MauroUr <74439705+MauroUr@users.noreply.github.com>
2026-04-15 19:50:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function typeWriter(text, i, currentKey) {
         output.textContent += text.charAt(i);
         
         i++;
-        typingTimeout = setTimeout(() => typeWriter(text, i, currentKey), 12); 
+        typingTimeout = setTimeout(() => typeWriter(text, i, currentKey), 6); 
         
         const consoleScreen = document.querySelector('.console-screen');
         consoleScreen.scrollTop = consoleScreen.scrollHeight;
